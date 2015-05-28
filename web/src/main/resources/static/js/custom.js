e69fd846e8071ddc2db716ed312d34b4bfecb99e
weicheng xi <xiwc87@yeah.net>
2015-05-28 22:50:42 +0800

    add markdown guide

--- web/src/main/resources/static/js/custom.js
+++ web/src/main/resources/static/js/custom.js
@@ -2,4 +2,19 @@ jQuery(document).ready(function($) {
 
     $('.ui.sidebar').sidebar({}).sidebar('attach events', '.xwc-menu');
     $('.ui.checkbox').checkbox();
+    $('.ui.accordion.i18n').accordion({
+        onOpen: function() {
+            $.get('md/i18n.md', function(data) {
+                $('.markdown-body').html(markdown.toHTML(data));
+            });
+        }
+    });
+    $('.ui.accordion.json2bean').accordion({
+        onOpen: function() {
+            $.get('md/json2bean.md', function(data) {
+                $('.markdown-body').html(markdown.toHTML(data));
+            });
+        }
+    });
+
 });
